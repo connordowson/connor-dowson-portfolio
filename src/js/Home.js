46240cb3a8dddc4937ec60aa0b9db8be020e1b40
Connor Dowson <connormwdowson@gmail.com>
2017-09-25 15:21:19 +0100

    Got react working

---
+++ src/js/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './../css/main.scss'
+
+export default class Home extends React.Component{
+
+    render(){
+
+        return(
+
+            <div id="home-container">
+
+                <nav> navbar </nav>
+
+
+            </div>
+
+        );
+
+    }
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,66 @@
+var path = require('path');
+var webpack = require('webpack');
+
+
 module.exports = {
 
-    entry: "./src/js/index.js"
+    entry: "./src/js/index.js",
+
+    output: {
+
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+
+    },
+
+    module: {
+
+        rules: [
+            
+            {
+
+                test: /\.scss$/,
+
+                use: [{
+
+                    loader: "style-loader" // creates style nodes from JS strings
+                },
+
+                {
+                    loader: "css-loader" // translates CSS into CommonJS
+                },
+
+                {
+                    loader: "sass-loader" // compiles Sass to CSS
+                }]
+
+            },
+
+            {
+
+                test: /\.js$/,
+
+                exclude: /(node_modules)/,
+
+                use: {
+
+                    loader: "babel-loader",
+
+                    options: {
+
+                        presets: ['es2015', 'react']
+                        
+                    }
+
+                }
+
+            }
+
+
+
+
+    
+        ]
+    }
 
 }
