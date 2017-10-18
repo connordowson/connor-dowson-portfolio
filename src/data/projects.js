e116b8b506ab35f629252de2760b2b045e7b32ae
Connor Dowson <connormwdowson@gmail.com>
2017-10-18 18:16:28 +0100

    Added Wall of Fame project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,11 +3,22 @@ const projects = [
     {
 
         title: "The Swamp Donkeys Website",
-        image: "tsd-image.jpeg",
+        image: "tsd-image1.jpg",
         description: "A website created for an esports team me and my friends created that we take too seriously. This was the first time I used React and Webpack for a project so was a big learning curve. I also created a news blog using Jekyll for updates to the team. The OpenDota API is used to display a win-loss graph of the DOTA 2 teams last 10 games. This was also my first time using FTP to upload a site to a webserver, which was another steep learning curve.",
-        skills: ['HTML5', 'CSS3', 'Webpack', 'ReactJS', 'Javascript'],
+        skills: ['HTML5', 'CSS3', 'Sass', 'Webpack', 'ReactJS', 'Javascript'],
         demo: "http://www.theswampdonkeys.com/",
         code: "https://github.com/connordowson"
+
+    },
+
+    {
+
+        title: "Music Wall of Fame",
+        image: "wall-of-fame.png",
+        description: "A project I created using the Discogs API. A NodeJS script downloads each album cover by using its ID, and then they are all displayed on the page. I learnt a lot about using Maps and Keys creating this project.",
+        skills: ['HTML5', 'CSS3', 'Sass', 'Webpack', 'ReactJS', 'NodeJS'],
+        demo: "http://www.connordowson.com/walloffame",
+        code: "https://github.com/connordowson/music-wall-of-fame"
 
     }
 
